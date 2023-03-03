fd4aa50bdd52b4a56cea14f3067e1266934530d4
Dipesh Singh <sinhdipesh@gmail.com>
2023-03-03 22:59:01 +0530

    updated

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
 
+# .bat files are used to automate task in windows like operation systems.
+
 mvn clean install -DskipTests assembly:single -q
 java -jar target\geektrust.jar sample_input\input1.txt
